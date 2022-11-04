c27ef1b63e6486fdca2f04b40407b3ffc566355b
Petras Jokubauskas <klavishas@gmail.com>
2022-11-04 17:12:13 +0100

    fix for PyQt5 v 5.15.6

--- lib/ui/CustomPGWidgets.py
+++ lib/ui/CustomPGWidgets.py
@@ -207,7 +207,7 @@ class CustomAxisItem(pg.AxisItem):
         # menu = self.scene().addParentContextMenus(self, menu, ev)
 
         pos = ev.screenPos()
-        menu.popup(QtCore.QPoint(pos.x(), pos.y()))
+        menu.popup(QtCore.QPoint(int(pos.x()), int(pos.y())))
         return True
 
     # This method will be called when this item's _children_ want to raise
